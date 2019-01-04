a99959cf835fbf124ace54d6a1c1d194f34fb581
610rodrigo <rodrigo.fla75@gmail.com>
2019-01-04 12:37:23 -0200

    File btn-top-contrle added

---
+++ docs/script/btn-top-controle.js
@@ -0,0 +1,17 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("topcontrole").style.display = "block";
+  } else {
+    document.getElementById("topcontrole").style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
