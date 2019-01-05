ba6300539b463acf582159d81de2bc543eff0486
610rodrigo <rodrigo.fla75@gmail.com>
2019-01-05 12:44:51 -0200

    Function scrollFunction added to file navigation

--- docs/script/navigation.js
+++ docs/script/navigation.js
@@ -1,4 +1,7 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {
+    myFunction()
+    scrollFunction()
+};
         
 var navMenu = document.getElementById("navMenu");
 var sticky = navMenu.offsetTop;
@@ -9,4 +12,19 @@ function myFunction() {
     } else {
     navMenu.classList.remove("sticky");
     }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("topcontrole").style.display = "block";
+  } else {
+    document.getElementById("topcontrole").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
